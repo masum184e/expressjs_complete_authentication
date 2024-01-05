ae28879c2026c1fc7f05dafb7534c62b49fd06a2
masum184e <masum184e@gmail.com>
2024-01-05 18:58:08 +0600

    admin auth added

---
+++ middleware/adminAuthentication.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+import UserModel from "../models/userSchema.js";
+
+const adminAuthentication = async (req, res, next) => {
+    try {
+        const cookieKey = process.env.EXPRESSJS_COMPLETE_AUTHENTICATION_TOKEN_COOKIE_KEY
+        const authorizationToken = req.cookies[cookieKey];
+        if (authorizationToken) {
+
+            const { adminId } = jwt.verify(authorizationToken, process.env.JWT_SECRET_KEY)
+            if (adminId) {
+
+                const admin = await UserModel.findById(adminId).select("-password");
+                if (admin.role === "admin") {
+                    req.admin = admin
+                    next();
+                } else {
+                    return res.status(403).json({ "status": false, "message": "Invalid Request" });
+                }
+
+            } else {
+                return res.status(403).json({ "status": false, "message": "Invalid Request" });
+            }
+
+        } else {
+            res.status(401).json({ "status": false, "message": "Authorization Failed" });
+        }
+
+    } catch (error) {
+        res.status(500).json({ "status": false, "message": "Internal Server Error", "error": error });
+    }
+}
+
+export default adminAuthentication;
