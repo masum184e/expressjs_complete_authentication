969fda33a768924bbdc84ea628dcb6e2eaf4e691
masum184e <masum184e@gmail.com>
2024-01-05 17:18:45 +0600

    user registration added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { userRegistration } from '../controller/userController.js';
 
 const userRouter = express.Router();
+userRouter.post("/registration", userRegistration);
 
 export default userRouter
