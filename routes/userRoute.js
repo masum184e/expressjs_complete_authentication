418f285f984f03da1f364803d37048d93b5ba8a2
masum184e <masum184e@gmail.com>
2024-01-05 18:59:40 +0600

    user data by id added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { userData, userLogin, userRegistration } from '../controller/userController.js';
+import { userData, userDataById, userLogin, userRegistration } from '../controller/userController.js';
 import userAuthentication from '../middleware/userAuthentication.js';
+import adminAuthentication from '../middleware/adminAuthentication.js';
 
 const userRouter = express.Router();
 userRouter.post("/registration", userRegistration);
 userRouter.post("/login", userLogin);
 userRouter.get("/", userAuthentication, userData);
+userRouter.get("/:userId", adminAuthentication, userDataById)
 
 export default userRouter
